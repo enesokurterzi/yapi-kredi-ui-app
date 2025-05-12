ae1b300f81e44f73925edf03929f80633f80aa7c
Enes OKURTERZİ <enes.okurterzi98@gmail.com>
2025-05-12 14:59:01 +0300

    add themes

---
+++ app/src/main/java/com/example/yapikredi/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.example.yapikredi.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFF179DE8)
+val PurpleGrey80 = Color(0xFF006FB7)
+val Pink80 = Color(0xFFEFB8C8)
+
+val PrimaryBlue = Color(0xFF179DE8)
+val PurpleGrey40 = Color(0xFF006FB7)
+val Pink40 = Color(0xFF7D5260)
+val SurfaceContainer = Color(0xFFECF4FB)
